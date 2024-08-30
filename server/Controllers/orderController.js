90d6c66c1790ecf3d91715ed145ec80047250809
Suresh <123563252+Sureshwebdeveloper@users.noreply.github.com>
2024-08-30 12:14:29 +0530

    Update frontend Path orderController.js

--- server/Controllers/orderController.js
+++ server/Controllers/orderController.js
@@ -7,8 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Place User Order for Frontend
 const placeOrder = async (req, res) => {
   // const frontend_url = process.env.CLIENT_URL;
-  const frontend_url = "http://localhost:5173";
-  console.log(frontend_url);
+  const frontend_url = "https://e-book-user.onrender.com/";
 
   try {
     const newOrder = orderModel({
